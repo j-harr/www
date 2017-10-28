bb664a2805c57c464890496f053cd4edfe5243be
jacob-harrison <jrhmtf@mst.edu>
2017-10-27 23:08:45 -0500

    Fixed home page margins

--- html/index.php
+++ html/index.php
@@ -9,48 +9,48 @@
         <header>
             <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
             text-align: left; padding: 0 20px 20px 20px;">jacobOL</h1>
-            <h3>Welcome to my site.</h3></h2>
+            <h3 style="text-align: right; padding: 0 20px 0 0;">Welcome to my site.</h3></h2>
             <?php
             echo "My first PHP script!";
             ?>
             <hr>
         </header>
-        <body>
-            <div class="ext_pgs">
-                <a href="Library/library.html">
-                    <div class="ext_page" style="background-color:lightgreen;
-                    color: black"">
-                        <div class="page_title">
-                            <h4>CS2300 Group 11 Library</h4>
-                        </div>
-                        <div class="page_description">
-                            <p>The home page for Group 11's simulated library database and website.
-                                 Likely under construction until Thanksgiving.</p>
-                        </div>
+        <div class="ext_pgs">
+            <a href="Library/library.html">
+                <div class="ext_page" style="background-color:lightgreen;
+                color: black">
+                    <div class="page-title">
+                        <h4>CS2300 Group 11 Library</h4>
                     </div>
-                </a>
-                <a href="About/about.html">
-                    <div class="ext_page" style="background-color:lightcoral;
-                    color: black"">
-                        <div class="page_title">
-                            <h4>About jacobOL</h4>
-                        </div>
-                        <div class="page_description">
-                            <p>Learn more about the jacobOL site and its humble beginnings.
-                                 Likely under construction until November 1, 2017.</p>
-                        </div>
+                    <div class="page-description">
+                        <p>The home page for Group 11's simulated library database and website.
+                             Likely under construction until Thanksgiving.</p>
                     </div>
-                </a>
+                </div>
+            </a>
+            <a href="About/about.html">
+                <div class="ext_page" style="background-color:lightcoral;
+                color: black">
+                    <div class="page-title">
+                        <h4>About jacobOL</h4>
+                    </div>
+                    <div class="page-description">
+                        <p>Learn more about the jacobOL site and its humble beginnings.
+                             Likely under construction until November 1, 2017.</p>
+                    </div>
+                </div>
+            </a>
+            <a>
                 <div class="ext_page" style="background-color:lightgoldenrodyellow;
                 color: black">
-                    <div class="page_title">
+                    <div class="page-title">
                         <h4>Mapping Project</h4>
                     </div>
-                    <div class="page_description">
+                    <div class="page-description">
                         <p>A project involving maps, graphs, networks, and databases. Likely under construction.</p>
                     </div>
                 </div>
-            </div>
-        </body>
+            </a>
+        </div>
     </body>
 </html>
