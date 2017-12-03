22e5279bb7a9e54aa9d07858295603f3a654f424
jacob-harrison <jrhmtf@mst.edu>
2017-12-03 15:25:39 -0600

    Updated page

---
+++ html/HMSystem/frmf1.php
@@ -0,0 +1,36 @@
+<?php
+//Declaration of variables
+$f1guestID = $f1guestFN = $f1guestMN = $f1guestLN = $f1guestPN = "";
+$f1guestSTR = $f1guestCTY = $f1guestSTE = $f1guestCTRY = $f1guestZIP = "";
+$f1guestAGE = $f1guestNIP = $f1guestRMN = $f1guestCHIN = $f1guestCHOT = "";
+$f1guestRATE = $f1guestECN = $f1guestECPHN = $f1guestECADR = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $f1guestID = clean($_POST["f1guestID"]);
+    $f1guestFN = clean($_POST["f1guestFN"]);
+    $f1guestMN = clean($_POST["f1guestMN"]);
+    $f1guestLN = clean($_POST["f1guestLN"]);
+    $f1guestPN = clean($_POST["f1guestPN"]);
+    $f1guestSTR = clean($_POST["f1guestSTR"]);
+    $f1guestCTY = clean($_POST["f1guestCTY"]);
+    $f1guestSTE = clean($_POST["f1guestSTE"]);
+    $f1guestCTRY = clean($_POST["f1guestCTRY"]);
+    $f1guestZIP = clean($_POST["f1guestZIP"]);
+    $f1guestAGE = clean($_POST["f1guestAGE"]);
+    $f1guestNIP = clean($_POST["f1guestNIP"]);
+    $f1guestRMN = clean($_POST["f1guestRMN"]);
+    $f1guestCHIN = clean($_POST["f1guestCHIN"]);
+    $f1guestCHOT = clean($_POST["f1guestCHOT"]);
+    $f1guestRATE = clean($_POST["f1guestRATE"]);
+    $f1guestECN = clean($_POST["f1guestECN"]);
+    $f1guestECPHN = clean($_POST["f1guestECPHN"]);
+    $f1guestECADR = clean($_POST["f1guestECADR"]);
+}
+
+function clean($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
