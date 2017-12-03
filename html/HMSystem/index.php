ba25e2e254c8d235422728745a4e877c29a48f63
jacob-harrison <jrhmtf@mst.edu>
2017-12-03 12:23:12 -0600

    Updated page

--- html/HMSystem/index.php
+++ html/HMSystem/index.php
@@ -42,7 +42,8 @@ echo "CONNECTED";
             <button class="menuButton f6btn" id="function6">Tender Payment</button>
         </div>
         <hr>
-        <form name="functionForm1" onsubmit="return validateForm()" method="post">
+        <form name="function1Form" onsubmit="return validateForm()" method="post">
+            <h3>Add Guest</h3>
             <div class="form-row">
                 <label>Guest ID</label><input type="text" name="f1guestID">
             </div>
@@ -56,7 +57,7 @@ echo "CONNECTED";
                 <label>Last Name</label><input type="text" name="f1guestLN">
             </div>
             <div class="form-row">
-                <label>Phone Number</label><input type="text" name="f1guestPN">
+                <label>Phone Number</label><input type="tel" name="f1guestPN">
             </div>
             <div class="form-row">
                 <label>Street</label><input type="text" name="f1guestSTR">
@@ -71,17 +72,42 @@ echo "CONNECTED";
                 <label>Country</label><input type="text" name="f1guestCTRY">
             </div>
             <div class="form-row">
-                <label>ZIP Code</label><input type="text" name="f1guestZIP">
+                <label>ZIP Code</label><input type="number" name="f1guestZIP">
             </div>
             <div class="form-row">
-                <label>Age</label><input type="text" name="f1guestAGE">
+                <label>Age</label><input type="number" name="f1guestAGE">
             </div>
             <div class="form-row">
-                <label># in Party</label><input type="text" name="f1guestNIP">
+                <label># in Party</label><input type="number" name="f1guestNIP">
+            </div>
+            <div class="form-row">
+                <label>Room Number</label><input type="number" name="f1guestRMN">
+            </div>
+            <div class="form-row">
+                <label>Check In</label><input type="date" name="f1guestCHIN">
+            </div>
+            <div class="form-row">
+                <label>Check Out</label><input type="date" name="f1guestCHOT">
+            </div>
+            <div class="form-row">
+                <label>Rate/night</label><input type="number" name="f1guestRATE">
+            </div>
+            <div class="form-row">
+                <h4>Emergency Contact<h4>
+            </div>
+            <div class="form-row">
+                <label>Name</label><input type="text" name="f1guestEC-N">
+            </div>
+            <div class="form-row">
+                <label>Phone Number</label><input type="tel" name="f1guestEC-PHN">
+            </div>
+            <div class="form-row">
+                <label>Address</label><input type="text" name="f1guestEC-ADR">
             </div>
             <div class="form-row">
                 <input class="submit" type="submit" value="Submit">
             </div>
+            
         </form>
 
     </body>
