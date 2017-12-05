345f03bb9e2256a264fd33dd68d9fb4a364aca8e
jacob-harrison <jrhmtf@mst.edu>
2017-12-04 21:43:49 -0600

    Updated page

--- html/HMSystem/frmf3.php
+++ html/HMSystem/frmf3.php
@@ -37,17 +37,18 @@ echo "<table border='1' >";
 while($data = $result->fetch_assoc())
 {
     echo "<tr>";
-    echo "<td>$data[0]</td>";
-    echo "<td>$data[1]</td>";
-    echo "<td>$data[2]</td>";
-    echo "<td>$data[3]</td>";
-    echo "<td>$data[4]</td>";
-    echo "<td>$data[5]</td>";
-    echo "<td>$data[6]</td>";
-    echo "<td>$data[7]</td>";
-    echo "<td>$data[8]</td>";
-    echo "<td>$data[9]</td>";
-    echo "<td>$data[10]</td>";
+    echo "<td>".$data[0]."</td>";
+    echo "<td>".$data[1]."</td>";
+    echo "<td>".$data[2]."</td>";
+    echo "<td>".$data[3]."</td>";
+    echo "<td>".$data[4]."</td>";
+    echo "<td>".$data[5]."</td>";
+    echo "<td>".$data[6]."</td>";
+    echo "<td>".$data[7]."</td>";
+    echo "<td>".$data[8]."</td>";
+    echo "<td>".$data[9]."</td>";
+    echo "<td>".$data[10]."</td>";
+    echo "</tr>";
 }
 echo "</table>";
 if ($conn->multi_query($sql) === TRUE) {
