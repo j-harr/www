af57bb565e87fc5a8cc71afee4b2f33ff0fde70c
jacob-harrison <jrhmtf@mst.edu>
2017-12-04 21:45:41 -0600

    Updated page

--- html/HMSystem/frmf3.php
+++ html/HMSystem/frmf3.php
@@ -37,8 +37,8 @@ echo "<table border='1' >";
 while($data = $result->fetch_assoc())
 {
     echo "<tr>";
-    echo "<td>".$data[0]."</td>";
-    echo "<td>".$data[1]."</td>";
+    echo "<td>".$data["GuestID"]."</td>";
+    echo "<td>".$data["F_Name"]."</td>";
     echo "<td>".$data[2]."</td>";
     echo "<td>".$data[3]."</td>";
     echo "<td>".$data[4]."</td>";
