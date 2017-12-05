11811c2a001fa0ece0023f0bdc5f88dcd24f12cf
jacob-harrison <jrhmtf@mst.edu>
2017-12-04 21:50:56 -0600

    Updated page

--- html/HMSystem/frmf3.php
+++ html/HMSystem/frmf3.php
@@ -27,9 +27,9 @@ function clean($data){
     return $data;
 }
 
-$sql = "SELECT * FROM Guest1 G1, Guest2 G2, Room R, Booking Bo
-WHERE G1.GuestID='".$f3guestID."' AND G1.GuestID=G2.GuestID AND
-G1.GuestID=Bo.GuestID AND Bo.RoomNumber=R.RoomNumber;";
+$sql = "SELECT * FROM Guest1 G1".//, Guest2 G2, Room R, Booking Bo
+"WHERE G1.GuestID='".$f3guestID."';";// AND G1.GuestID=G2.GuestID AND
+//G1.GuestID=Bo.GuestID AND Bo.RoomNumber=R.RoomNumber;";
 
 $result = $conn->query($sql);
 echo "<table border='1' >";
